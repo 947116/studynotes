5af45113b0de18536cdb0d7ca625219c713f7f4c
947116 <947116@gmail.com>
2023-12-07 13:21:03 +0100

    postponed

---
+++ 30_Days_Python/day6.py
@@ -0,0 +1,6 @@
+# I need to pause this project
+# Was the most fun within the last few months
+# But I need to fix some stuff before that (GC + C)
+# After that I will be back. One day I am gonna be able to code :)
+# Maybe I find time during weekends; maybe after 2025
+# Lets see; see you FR
